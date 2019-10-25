964aca68654acd0ea1e08d83333c8ced0e673842
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-10-25 20:00:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -24,23 +24,3 @@ class BankAccount
     @status = "closed"
   end
 end
-  def balance=(balance)
-    @balance
-  end
-
-  def valid?
-    (!!status == 'open') && (!!balance > 0)
-  end
-
-def account_hash
-    instance_variables.map do |var|
-        [var[1..-1].to_sym, instance_variable_get(var)]
-    end.to_h
-  end
-
-end
-
-
-
- avi = BankAccount.new("Avi")
- avi.account_hash
